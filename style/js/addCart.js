4b863f0db7e267d5aa2bf7abd51e725c30c3602c
Mcebo Samuel <Mcebo Samuel Xaba>
2020-12-22 16:27:34 +0200

    fixed the checkout and checkin buttons

--- style/js/addCart.js
+++ style/js/addCart.js
@@ -1,27 +1,24 @@
-//this script is used to control the add to cart section of the web page as well as the modal
-
-//To implement
 function removeFocus() {
-    console.log("removing focus");
     document.getElementById('add-to-cart').focusout()
 }
 
 function enableAddToCart(){
     if(document.getElementById('color-change').innerText !== 'Select A color'){
-        document.getElementById('add-to-cart').disabled = false;
+        document.getElementById('add-to-cart').hidden = false;
+        document.getElementById('btn-checkout').hidden = true;
     }
     else{
-        document.getElementById('add-to-cart').disabled = true;
+        document.getElementById('add-to-cart').hidden = true;
+        document.getElementById('btn-checkout').hidden = false;
     }
 }
 
 function disableAddToCart(){
-    document.getElementById('add-to-cart').disabled = true;
+    document.getElementById('add-to-cart').hidden = true;
+    document.getElementById('btn-checkout').hidden = false;
     document.getElementById('color-change').innerText = 'Select A color';
 }
 
-// -------------------- Modal Functions -------------------
-//Increase Quantity of Item
 function incrementQuantity(){
     let quantity = document.getElementById("add-quantity").innerHTML;
     document.getElementById("add-quantity").innerHTML = ++quantity;
@@ -30,7 +27,6 @@ function incrementQuantity(){
 
 }
 
-//Decrease Quantity of Item
 function decrementQuantity(){
     let quantity = document.getElementById("add-quantity").innerHTML;
     if(quantity>0){
