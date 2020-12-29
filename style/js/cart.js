793a2323c7ef84888034bd8233d3d84918ba3337
Mcebo Samuel <Mcebo Samuel Xaba>
2020-12-29 09:57:37 +0200

    comments

--- style/js/cart.js
+++ style/js/cart.js
@@ -1,112 +1,38 @@
-const colorItem = {
-    "itemName": "HICKIES Originals",
-    "itemRating": "1,293 Reviews",
-    "discounted": 14.99,
-    "realPrice": 19.99,
-    "discountPecentage": "25% OFF",
-    "colors": {
-        "yellowgreen": {
-            "display-name": "Yellowgreen",
-            "price": 14.99
-        },
-        "red": {
-            "display-name": "Red",
-            "price": 20.99
-        },
-        "peru": {
-            "display-name": "Peru",
-            "price": 15.99
-        },
-        "white": {
-            "display-name": "White",
-            "price": 19.01
-        },
-        "seashell": {
-            "display-name": "Seashell",
-            "price": 18.99
-        },
-        "olive": {
-            "display-name": "Olive",
-            "price": 21.99
-        },
-        "black": {
-            "display-name": "Black",
-            "price": 14.99
-        },
-        "aqua": {
-            "display-name": "Aqua",
-            "price": 14.99
-        },
-        "gold": {
-            "display-name": "Gold",
-            "price": 22.99
-        },
-        "powderblue": {
-            "display-name": "Powder-Blue",
-            "price": 24.99
-        },
-        "whitesmoke": {
-            "display-name": "White Smoke",
-            "price": 12.01
-        },
-        "salmon": {
-            "display-name": "Salmon",
-            "price": 24.99
-        },
-        "darkorchid": {
-            "display-name": "Darkorchid",
-            "price": 29.99
-        },
-        "burlywood": {
-            "display-name": "Burlywood",
-            "price": 19.99
-        },
-        "hotpink": {
-            "display-name": "Hotpink",
-            "price": 14.99
-        },
-        "maroon": {
-            "display-name": "Maroon",
-            "price": 14.99
-        },
-        "slategray": {
-            "display-name": "Slategray",
-            "price": 49.99
-        },
+function decrement(){
+    let quantity = document.getElementById("add-quantity").innerHTML;
+    if(quantity>0){
+        document.getElementById("add-quantity").innerHTML = --quantity;
     }
-};
 
-function getColorItem() {
-    return colorItem;
-}
-
-function initialisePrices(){
-    let itemPrice = document.getElementById('discounted');
-    let itemrealPrice = document.getElementById('no-discount');
-    let itemDiscount = document.getElementById('discount');
-
-    itemPrice.innerText = '$' + getDiscountedPrice();
-    itemrealPrice.innerText = '$' + getRealPrice();
-    itemDiscount.innerText = getDiscountPecentage();
-    
-    initialiseColors()
+    let updatedQuantity = document.getElementById("add-quantity").innerHTML;
+    console.log(updatedQuantity);
+    if(updatedQuantity==0){
+        document.getElementById("qnty-accept").disabled = true;
+    }
 
 }
 
-
-function getDiscountedPrice(){
-    return colorItem['discounted']
+function enableAddCart(){
+    if(document.getElementById('change-color').innerText !== 'Select A color'){
+        document.getElementById('add-to-cart').hidden = false;
+        document.getElementById('btn-checkout').hidden = true;
+    }
+    else{
+        document.getElementById('add-to-cart').hidden = true;
+        document.getElementById('btn-checkout').hidden = false;
+    }
 }
 
-function getRealPrice(){
-    return colorItem['realPrice']
+function increment(){
+    let quantity = document.getElementById("add-quantity").innerHTML;
+    document.getElementById("add-quantity").innerHTML = ++quantity;
+    document.getElementById("qnty-accept").disabled = false;
+
+
 }
 
-function getDiscountPecentage(){
-    return colorItem['discountPecentage']
-}
-
-function refresh(){
-    alert("See you on your next Shopping!");
-    location.reload();
+function disableAddCart(){
+    document.getElementById('add-to-cart').hidden = true;
+    document.getElementById('btn-checkout').hidden = false;
+    document.getElementById('change-color').innerText = 'Select A color';
 }
